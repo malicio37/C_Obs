48957200ebd89cc74ea4fd93a5413a33baba7169
Julián Mauricio Mejía Cardona <jmmejia@autonoma.edu.co>
2016-06-15 18:10:37 -0500

    Tabla estado competencia terminada

--- frontend/js/my-app.js
+++ frontend/js/my-app.js
@@ -259,7 +259,6 @@ myApp.onPageInit('escanear', function (page) {
       * carga de datos del QR
       */
 
-      var codigo= 0.6843510080914488;
       if(nodo_id=="" || codigo==null){
         myApp.alert('No tiene pistas pendientes para seguir');
       }
@@ -303,6 +302,58 @@ myApp.onPageInit('escanear', function (page) {
       });
       }
     });
+
+    pageContainer.find('.botonPasarCodigo').on('click', function () {
+        var nodo_id= pageContainer.find('select[name="pistasQR"]').val();
+
+        /**
+        * carga de datos del QR
+        */
+        var codigo = pageContainer.find('input[name="codeText"]').val();
+        if(nodo_id=="" || codigo==""){
+          myApp.alert('No tiene pistas pendientes para seguir');
+        }
+        else{
+        var params= '{"node_id":' + nodo_id + ', "code":"' + codigo + '"}';
+          $$.post(backend +'/nodes/validate', params, function (data) {
+            var arreglo=JSON.parse(data);
+          if(data == 'false'){
+            myApp.alert('El código no corresponde a la pista actual ');
+          }
+          else{
+            //obtener el id del nodo descubierto a actualizar
+
+            $$.get(backend +'/nodesdiscovered/' + user + '/' + nodo_id,  function (data) {
+              var nodoDescubierto=JSON.parse(data);
+              var nd_id= nodoDescubierto.id;
+              var nd_question_id= nodoDescubierto.question_id;
+              var nd_statusDate1= nodoDescubierto.statusDate1;
+              //put a estado 1
+              var params = '{"node_id":'+ nodo_id + ', "user_id":'+user+', "question_id":' + nd_question_id +', "status":1'
+                            +', "statusDate1":"'+nd_statusDate1+'","statusDate2":"'+getActualDateTime()+
+                            '","statusDate3":null }';
+              $$.ajax({
+                 url: backend + '/nodesdiscovered/'+nd_id,
+                 type: "PUT",
+                 contentType: "application/json",
+                 data: params,
+                 success: function(data, textStatus ){
+                   data = JSON.parse(data);
+                   myApp.alert('Tienes una nueva pregunta!!');
+                   //lo envìa a la página de ver pista a ver la nueva pista generada
+                   mainView.router.loadPage("principal.html");
+                 },
+                 error: function(xhr, textStatus, errorThrown){
+                   // We have received response and can hide activity indicator
+                   console.log('fallo al actualizar nodo descubierto');
+                 }
+              });
+            });
+          }
+        });
+        }
+      });
+
   });
 
 
@@ -466,13 +517,19 @@ myApp.onPageInit('score', function (page) {
     if(longitud == 0){
       myApp.alert('Ningún participante tiene nodos visitados aún!! ');
     }
-    if(longitud < 4){
-      top=Object.keys(arreglo).length;
+    else{
+      if(longitud < 4){
+        top=Object.keys(arreglo).length;
+      }
+
+      //generar la tabla del ranking
+
+      texto='<table>	<tr>		<td>Posición</td>		<td>Usuario</td>		<td>Cant. Nodos Visitados</td>	</tr>';
+      for(i=0;i < top; i++){
+        texto+= '<tr><td>'+ (i+1) + '</td><td>' + arreglo[i].email + '</td><td>'+ arreglo[i].cantidad+ '</td></tr>';
+      }
+      document.getElementById("totalScore").innerHTML = texto;
     }
-    for(i=0;i < top; i++){
-      texto+= "Posición "+ (i+1) + " | " + arreglo[i].email + " | "+ arreglo[i].cantidad+" nodos completados " +'<br>';
-    }
-    document.getElementById("totalScore").innerHTML = texto;
   });
 });
 
